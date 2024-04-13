3a1be1dbca8fc8b4568dd00efcb5b0847beae87f
maribelniok <mariannabelniok@outlook.com>
2024-04-13 11:44:00 -0300

    ADD: raise error when email is not provided in user creation

--- app/core/models.py
+++ app/core/models.py
@@ -9,14 +9,16 @@ from django.contrib.auth.models import (
 
 
 class UserManager(BaseUserManager):
-		"""Manager for users."""
+    """Manager for users."""
 
-		def create_user(self, email, password=None, **extra_fields):
-				"""Create, save and return a new user."""
-				user = self.model(email=self.normalize_email(email), **extra_fields)
-				user.set_password(password)
-				user.save(using=self._db)
-				return user
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a new user."""
+        if not email:
+            raise ValueError('User must have an email address.')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -25,7 +27,5 @@ class User(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-
     objects = UserManager()
-
     USERNAME_FIELD = 'email'
