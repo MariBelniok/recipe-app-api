9083f5f2b458694a0b80eb7bf15d4d6bb5b4201d
maribelniok <mariannabelniok@outlook.com>
2024-04-26 07:08:20 -0300

    ADD: recipe model and listing

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -7,6 +7,14 @@ from django.contrib.auth import get_user_model
 from core import models
 
 
+def create_user(email='user@example.com', password='testpass123'):
+    """Create a sample user."""
+    return get_user_model().objects.create_user(
+        email=email,
+        password=password,
+    )
+
+
 class ModelTest(TestCase):
     """Test models."""
 
@@ -61,3 +69,9 @@ class ModelTest(TestCase):
             description='Sample receipe description.',
         )
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """Test creating a tag is successful."""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Sample tag')
+        self.assertEqual(str(tag), tag.name)
